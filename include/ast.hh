633af9b832c17f03fbe4f057be758c6bf3ebf946
Kian Shepherd <kshepherd@kx.com>
2022-10-02 02:12:20 -0400

    Add scope frame class

--- include/ast.hh
+++ include/ast.hh
@@ -3,9 +3,25 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <map>
 #include "expression.hh"
 #include "statement.hh"
 #include "function.hh"
+
+class ScopeFrame {
+public:
+    ScopeFrame();
+    ScopeFrame(ScopeFrame* prev);
+    ScopeFrame* new_scope();
+
+    Expression* get_value(std::string identifier);
+    void push_value(std::string identifier, Expression* expression);
+
+    ScopeFrame* prev_frame;
+    ScopeFrame* next_frame;
+private:
+    std::map<std::string, Expression*> variables;
+};
 
 class AST {
 public:
@@ -14,6 +30,9 @@ public:
     void push_function(Function* function);
     void push_err(std::string msg);
 
+    void push_scope();
+    void pop_scope();
+
     bool check_error(std::string stage);
     void debug();
     void fold_const_expressions();
@@ -21,4 +40,6 @@ private:
     std::vector<Function*> functions;
     bool error;
     std::stringstream err;
+
+    ScopeFrame* scope;
 };
--- src/ast.cc
+++ src/ast.cc
@@ -4,6 +4,7 @@ AST::AST() {
     this->functions = std::vector<Function*>();
     this->error = false;
     this->err = std::stringstream();
+    this->scope = new ScopeFrame();
 }
 
 void AST::push_function(Function* function) {
@@ -34,4 +35,50 @@ void AST::fold_const_expressions() {
     for (int i = 0; i < (int)this->functions.size(); i++) {
         this->functions[i]->fold(this);
     }
+}
+
+void AST::push_scope() {
+    this->scope = this->scope->new_scope();
+}
+
+void AST::pop_scope() {
+    if (this->scope->prev_frame == nullptr) {
+        this->push_err("Popped too many scope frames.");
+        this->check_error("Scope Error: ");
+    } else {
+        this->scope = this->scope->prev_frame;
+        delete this->scope->next_frame;
+    }
+}
+
+ScopeFrame::ScopeFrame() {
+    this->prev_frame = nullptr;
+    this->next_frame = nullptr;
+    this->variables = std::map<std::string, Expression*>();
+}
+
+ScopeFrame::ScopeFrame(ScopeFrame* prev) {
+    this->prev_frame = prev;
+    this->next_frame = nullptr;
+    this->variables = std::map<std::string, Expression*>();
+}
+
+ScopeFrame* ScopeFrame::new_scope() {
+    ScopeFrame* next = new ScopeFrame(this);
+    this->next_frame = next;
+    return next;
+}
+
+void ScopeFrame::push_value(std::string identifier, Expression* expression) {
+    this->variables[identifier] = expression;
+}
+
+Expression* ScopeFrame::get_value(std::string identifier) {
+    auto iter = this->variables.find(identifier);
+    if (iter != this->variables.end() ) {
+        return iter->second;
+    }
+    if (this->prev_frame == nullptr)
+        return nullptr;
+    return this->prev_frame->get_value(identifier);
 }
