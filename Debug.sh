84e250a1a1cab00a9ce741939f994356d00e1184
Kian Shepherd <kshepherd@kx.com>
2022-10-08 16:42:44 -0400

    Update Debug build

--- Debug.sh
+++ Debug.sh
@@ -2,4 +2,4 @@
 flex -d src/tokens.l
 bison --debug -Dparse.trace -v src/grammar.y
 
-g++ --std=c++11 -DDEBUGGING -Wall -I ./include src/dusk.cc src/parser.cc src/scanner.cc src/ast.cc src/expression.cc src/statement.cc src/function.cc -o dusk
+g++  -Wall -I ./include src/dusk.cc src/parser.cc src/scanner.cc src/ast.cc src/expression.cc src/statement.cc src/function.cc `llvm-config --cxxflags --ldflags --system-libs --libs all` -o dusk
