9b48a6ff9857aced84dd525f7629d3e941f35a85
Kian Shepherd <kshepherd@kx.com>
2022-10-10 22:58:25 -0400

    Seperate build.sh into run.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,3 @@ cmake -G Ninja -S . -B CMake
 ninja -C CMake
 mv CMake/Dusk dusk
 mv CMake/libdusk.so libdusk.so
-
-./dusk test.ds
-g++ output.o -L. -ldusk -o out
-rm output.o
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./ ./out
