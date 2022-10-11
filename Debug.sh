6e09bfffa159a357651c58ffb716ca2ed93a9553
Kian Shepherd <kshepherd@kx.com>
2022-10-10 22:30:20 -0400

    Remove Debug script

--- Debug.sh
+++
@@ -1,5 +0,0 @@
-./clean.sh
-flex -d src/tokens.l
-bison --debug -Dparse.trace -v src/grammar.y
-
-g++  -Wall -I ./include src/dusk.cc src/parser.cc src/scanner.cc src/ast.cc src/expression.cc src/statement.cc src/function.cc `llvm-config --cxxflags --ldflags --system-libs --libs all` -o dusk
