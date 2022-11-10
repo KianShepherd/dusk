5c0eccd325467f49f6f282b9985e6934000b8462
kian_shepherd <kshepherd@kx.com>
2022-11-10 00:34:35 -0500

    I/O tests and refactor test script

--- ptest.py
+++ ptest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import subprocess
+import time
 
 from os.path import isfile, join
 
@@ -8,8 +9,39 @@ example_path = './examples/'
 
 test_cases = [str(f).split('.')[0] for f in os.listdir(example_path) if isfile(join(example_path, f))]
 
-os.system('./clean.sh')
-os.system('./build.sh')
+print('Cleaning old files.')
+subprocess.call(['sh', f'./clean.sh'],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
+print('Done.')
+print('Building dusk.')
+s = time.time()
+subprocess.call(['sh', f'./build.sh'],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+)
+e = time.time()
+print(f'Done, took {e - s} seconds.')
+
+
+def run_output(file_name, *, opt=None, expected=False):
+    if expected:
+        with open(f'./examples/example_outputs/{file_name}.txt') as f:
+            return f.read()
+
+    command = [f'./CMake/dusk']
+    if not opt is None:
+        command.append('-O')
+    command.append(f'./examples/{file_name}.ds')
+    subprocess.run(command,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    if file_name == 'io':
+        return subprocess.run(['./out'], stdout=subprocess.PIPE, input='Hello input()', text=True).stdout
+    else:
+        return subprocess.run(['./out'], stdout=subprocess.PIPE, text=True).stdout
 
 
 def run_example(file_name):
@@ -17,27 +49,20 @@ def run_example(file_name):
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
     )
-    no_op_res = subprocess.run(['./out'], stdout=subprocess.PIPE, text=True).stdout
-    subprocess.run([f'./CMake/dusk', '-O', f'./examples/{file_name}.ds'],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
-    op_res = subprocess.run(['./out'], stdout=subprocess.PIPE, text=True).stdout
+    no_op_res = run_output(file_name)
+    op_res = run_output(file_name, opt=1)
+    expected = run_output(file_name, expected=True)
 
-    expected = ''
-    with open(f'./examples/example_outputs/{file_name}.txt') as f:
-        expected = f.read()
-
-    print(f'Unoptimized {file_name}.ds: ', end='')
-    if no_op_res != expected:
-        print('FAIL')
-    else:
-        print('PASS')
-    print(f'Optimized {file_name}.ds: ', end='')
-    if op_res != expected:
-        print('FAIL')
-    else:
-        print('PASS')
+    no_op_pass = no_op_res == expected
+    op_pass = op_res == expected
+    preamble = f'{file_name}.ds: '
+    while len(preamble) < 20:
+        preamble = preamble + " "
+    print(f'{preamble}unoptimized {"PASS" if no_op_pass else "FAIL"}, optimized {"PASS" if no_op_pass else "FAIL"}')
+    if not (no_op_pass and op_pass):
+        print(op_res)
+        print(no_op_res)
+        print(expected)
 
 
 for f in test_cases:
