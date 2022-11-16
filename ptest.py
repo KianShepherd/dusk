64e950566f16d8e2a78da228c47edee71c49edbb
Kian Shepherd <kshepherd@kx.com>
2022-11-16 00:32:05 -0500

    ptest

--- ptest.py
+++ ptest.py
@@ -83,14 +83,15 @@ def run_example(file_name):
         while len(preamble) < 20:
             preamble = preamble + " "
         print(f'{preamble}unoptimized {"PASS" if no_op_pass else "FAIL"},'
-              f' optimized {"PASS" if no_op_pass else "FAIL"}')
+              f' optimized {"PASS" if op_pass else "FAIL"}')
         if not (no_op_pass and op_pass):
             print("\nOptimized\n")
             print(op_res)
             print("\nUnoptimized\n")
             print(no_op_res)
-            print("\nExprected\n")
+            print("\nExpected\n")
             print(expected)
+            print('')
     except BaseException:
         print(f'Failed to compile {file_name}')
 
