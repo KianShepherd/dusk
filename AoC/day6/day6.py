ca90531c14519ff762125b1a0fcb77d15293d616
kian_shepherd <kshepherd@kx.com>
2022-12-09 00:56:47 -0500

    Day6

--- AoC/day6/day6.py
+++ AoC/day6/day6.py
@@ -1,9 +1,38 @@
 """AoC day 6."""
 
 
-contents = []
+contents = None
 with open('./AoC/day6/input.txt', 'r') as f:
-    [contents.append(x) for x in f.readlines()]
+    contents = f.read().strip()
 
-print(f'Part 1: {0}')
-print(f'Part 2: {0}')
+
+def get_packet_start(stream):
+    """Get packet start."""
+    for i in range(len(stream) - 3):
+        letters = {}
+        unique = True
+        for j in range(4):
+            if stream[i + j] in letters.keys():
+                unique = False
+                break
+            letters[stream[i + j]] = 1
+        if unique:
+            return i + 4
+
+
+def get_message_start(stream):
+    """Get message start."""
+    for i in range(len(stream) - 13):
+        letters = {}
+        unique = True
+        for j in range(14):
+            if stream[i + j] in letters.keys():
+                unique = False
+                break
+            letters[stream[i + j]] = 1
+        if unique:
+            return i + 14
+
+
+print(f'Part 1: {get_packet_start(contents)}')
+print(f'Part 2: {get_message_start(contents)}')
